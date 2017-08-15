af0af0febb16691a36a599b042ed6cfc0a7e6080
Kevin Wong <k.wong4@yahoo.com>
2017-08-15 18:48:22 -0400

    title and hero property added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
