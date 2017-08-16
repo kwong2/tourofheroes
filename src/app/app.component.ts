25cba2299ca1a2c6aa354a48598cd1c116edf8ce
Kevin Wong <k.wong4@yahoo.com>
2017-08-15 22:34:32 -0400

    Hero Editor added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  template: 
+    `<h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder = "name">
+    </div>`
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
